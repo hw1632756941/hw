571f3455553c2ca85ec065f5eb6494a10f946606
hw <1632756941@qq.com>
2021-05-09 14:22:50 +0800

    修改

--- git.java
+++ git.java
@@ -1,5 +1,6 @@
 public class Github{
   public static void main(String[] args){
-     System.out.println("hello world!");   
+     System.out.println("hello world!");
+     System.out.println("加油啊！");
   }
 }
