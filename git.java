7fcde8b1f79dd121acc7298e0e0380fc629b9d1c
hw <1632756941@qq.com>
2021-05-09 14:02:40 +0800

    提交Java文件

--- git.java
+++ git.java
@@ -1,5 +1,5 @@
 public class Github{
-   public static void main(String[] args){
-      System.out.println("hello world ! i love git !");   
-   }
+  public static void main(String[] args){
+     System.out.println("hello world!");   
+  }
 }
